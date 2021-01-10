991c4f2f4c406bd4b99e42bf8fcc2bddea10f4f0
MRonald <ronaldmichael918@gmail.com>
2021-01-10 16:59:35 -0300

    Finalizando retornaIndice() e começando a função para encriptar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var btnEncriptar = document.getElementById("encriptar");
 var btnDecriptar = document.getElementById("decriptar");
 var btnSetChave = document.getElementById("setChave");
 var btnSetSenha = document.getElementById("setSenha");
+var campoEncriptar = document.getElementById("txtEncriptar");
+var campoDecriptar = document.getElementById("txtDecriptar");
 var senha = "";
 var chave = "";
 // Matriz de criptografia
@@ -42,16 +44,93 @@ btnSetSenha.addEventListener('click', setSenha);
 
 // Requisitos iniciais do software
 window.onload = function () {
-    setSenha();
-    setChave();
+    //setSenha();
+    //setChave();
 }
 
 // Functions
 function encriptarTxt() {
     // Para criptografar o texto escrito
+    let lc = 0;
+    let iLetraTxt = 0;
+    let iLetraChave = 0;
+    let txtEncriptar = campoEncriptar.value.toUpperCase();
+    let txtDecriptar = "";
+    alert(txtEncriptar.replace(/ /g,""));
+    for (let l of txtEncriptar) {
+        if (l == " ") {
+            txtDecriptar += " ";
+        } else {
+            iLetraTxt = retornaIndice(l);
+            iLetraChave = retornaIndice(chave[lc]);
+            txtDecriptar += vigenere[iLetraTxt][iLetraChave];
+            if (lc == chave.length - 1) {
+                lc = 0;
+            } else {
+                lc++;
+            }
+        }
+    }
+    campoDecriptar.value = txtDecriptar;
 }
 function decriptarTxt() {
     // Para descriptografar o texto escrito
+}
+function retornaIndice(letra) {
+    // Retorna o índice da letra de acordo com o alfabeto
+    if (letra == "A") {
+        return 0;
+    } else if (letra == "B") {
+        return 1;
+    } else if (letra == "C") {
+        return 2;
+    } else if (letra == "D") {
+        return 3;
+    } else if (letra == "E") {
+        return 4;
+    } else if (letra == "F") {
+        return 5;
+    } else if (letra == "G") {
+        return 6;
+    } else if (letra == "H") {
+        return 7;
+    } else if (letra == "I") {
+        return 8;
+    } else if (letra == "J") {
+        return 9;
+    } else if (letra == "K") {
+        return 10;
+    } else if (letra == "L") {
+        return 11;
+    } else if (letra == "M") {
+        return 12;
+    } else if (letra == "N") {
+        return 13;
+    } else if (letra == "O") {
+        return 14;
+    } else if (letra == "P") {
+        return 15;
+    } else if (letra == "Q") {
+        return 16;
+    } else if (letra == "R") {
+        return 17;
+    } else if (letra == "S") {
+        return 18;
+    } else if (letra == "T") {
+        return 19;
+    } else if (letra == "U") {
+        return 20;
+    } else if (letra == "V") {
+        return 21;
+    } else if (letra == "W") {
+        return 22;
+    } else if (letra == "X") {
+        return 23;
+    } else if (letra == "Y") {
+        return 24;
+    } else if (letra == "Z") {
+        return 25;
+    }
 }
 function setChave() {
     // Para mudar chave da criptografia
@@ -69,6 +148,7 @@ function setChave() {
             alert("Nova chave definida.");
         }
     }
+    chave = chave.toUpperCase();
 }
 function setSenha() {
     // Configura a senha de usuário
@@ -88,8 +168,8 @@ function setSenha() {
         }
     } else {
         // Configurando a senha no caso de não ser o primeiro acesso
-        let entSenha = prompt("Informe a senha atual:");
-        if (entSenha == senha) {
+        let reqSenha = prompt("Informe a senha atual:");
+        if (reqSenha == senha) {
             while (!valido) {
                 let s1 = prompt("Crie uma senha:");
                 let s2 = prompt("Confirme a senha:");
